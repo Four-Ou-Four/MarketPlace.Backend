27da417761bc3a01b4f02f5b8d6977ad716417b9
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-16 23:26:33 +0500

    Created identity entities

---
+++ MarketPlace.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using MarketPlace.Domain.Common.Entities;
+using MarketPlace.Domain.Enums;
+
+namespace MarketPlace.Domain.Entities;
+
+public class User : AuditableEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string PasswordHash { get; set; }
+    public int Age { get; set; }
+    public bool IsEmailAddressVerified { get; set; }
+    public Role Role { get; set; }
+    public UserSettings? UserSettings { get; set; }
+    public IEnumerable<Product> Products { get; set; }
+    public IEnumerable<Order> Orders { get; set; }
+    public Cart Cart { get; set; }
+}
---
+++ MarketPlace.Domain/Entities/UserSettings.cs
@@ -0,0 +1,14 @@
+﻿using MarketPlace.Domain.Common.Entities;
+using MarketPlace.Domain.Enums;
+
+namespace MarketPlace.Domain.Entities;
+
+public class UserSettings : IEntity
+{
+    public NotificationType? PreferredNotificationType { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the user Id
+    /// </summary>
+    public Guid Id { get; set; }
+}
