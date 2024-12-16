181f0197c035a2007535fb48feb40d4c0d40a882
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-16 23:25:59 +0500

    Created verification entities

---
+++ MarketPlace.Domain/Entities/UserInfoVerificationCode.cs
@@ -0,0 +1,13 @@
+﻿using MarketPlace.Domain.Enums;
+
+namespace MarketPlace.Domain.Entities;
+
+public class UserInfoVerificationCode : VerificationCode
+{
+    public UserInfoVerificationCode()
+    {
+        Type = VerificationType.UserInfoVerificationCode;
+    }
+
+    public Guid UserId { get; set; }
+}
---
+++ MarketPlace.Domain/Entities/VerificationCode.cs
@@ -0,0 +1,19 @@
+﻿using MarketPlace.Domain.Common.Entities;
+using MarketPlace.Domain.Enums;
+
+namespace MarketPlace.Domain.Entities;
+
+public abstract class VerificationCode : Entity
+{
+    public VerificationCodeType CodeType { get; set; }
+
+    public VerificationType Type { get; set; }
+
+    public DateTimeOffset ExpiryTime { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string Code { get; set; } = default!;
+
+    public string VerificationLink { get; set; } = default!;
+}
