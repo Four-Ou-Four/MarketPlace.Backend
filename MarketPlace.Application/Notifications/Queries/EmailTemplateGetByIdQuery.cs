01e8549cadca88e3ee38fa6dcc1ab94d6917ace6
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:04:49 +0500

    Created email template get by id query

---
+++ MarketPlace.Application/Notifications/Queries/EmailTemplateGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Application.Notifications.Models;
+
+namespace MarketPlace.Application.Notifications.Queries;
+
+public class EmailTemplateGetByIdQuery : IQuery<EmailTemplateDto?>
+{
+    public Guid EmailTemplateId { get; set; }
+}
