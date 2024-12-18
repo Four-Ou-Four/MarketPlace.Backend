d32ec2a636c87e20cd3015675aff58969d01e1cd
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:06:14 +0500

    Created user get by id query

---
+++ MarketPlace.Application/Identity/Queries/UserGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Application.Identity.Models;
+
+namespace MarketPlace.Application.Identity.Queries;
+
+public class UserGetByIdQuery : IQuery<UserDto?>
+{
+    public Guid UserId { get; set; }
+}
