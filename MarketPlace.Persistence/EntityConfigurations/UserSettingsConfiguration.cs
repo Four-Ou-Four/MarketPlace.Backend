51b95e85d0d654d8064611418d3e9991e71a5032
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:05:33 +0500

    Created user settings configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/UserSettingsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class UserSettingsConfiguration : IEntityTypeConfiguration<UserSettings>
+{
+    public void Configure(EntityTypeBuilder<UserSettings> builder)
+    {
+        builder.HasOne<User>().WithOne(user => user.UserSettings).HasForeignKey<UserSettings>(settings => settings.Id);
+    }
+}
