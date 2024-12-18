0eea3eac9ef0468167b27b71fcce1c4973bbe0ad
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:10:57 +0500

    Created cart item dto

---
+++ MarketPlace.Application/Carts/Models/CartItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MarketPlace.Application.Carts.Models;
+
+public class CartItemDto
+{
+    public Guid? Id { get; set; }
+    public Guid CartId { get; set; }
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+}
