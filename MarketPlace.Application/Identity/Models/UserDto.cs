f5dbdf054f407778f4e784b48d86c5314e88124f
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:12:45 +0500

    Created user dto

---
+++ MarketPlace.Application/Identity/Models/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using MarketPlace.Domain.Enums;
+
+namespace MarketPlace.Application.Identity.Models;
+
+public class UserDto
+{
+    public Guid? Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string PasswordHash { get; set; }
+    public int Age { get; set; }
+    public Role Role { get; set; }
+}
