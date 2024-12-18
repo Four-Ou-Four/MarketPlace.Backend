7d52ee0aab7bc1953433975c4efdf4f2e8dd7eea
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:00:36 +0500

    Created email sender interface

---
+++ MarketPlace.Application/Notifications/Services/IEmailSenderService.cs
@@ -0,0 +1,8 @@
+﻿using MarketPlace.Application.Notifications.Models;
+
+namespace MarketPlace.Application.Notifications.Services;
+
+public interface IEmailSenderService
+{
+    ValueTask<bool> SendAsync(EmailMessage emailMessage, CancellationToken cancellationToken = default);
+}
