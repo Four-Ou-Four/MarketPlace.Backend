0c8a0d99c5cb2ce44edc3da25860b07171aba2c4
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:01:50 +0500

    Created user settings interface

---
+++ MarketPlace.Application/Identity/Services/IUserSettingsService.cs
@@ -0,0 +1,18 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Application.Identity.Services;
+
+public interface IUserSettingsService
+{
+    ValueTask<UserSettings?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<UserSettings> CreateAsync(
+        UserSettings userSettings,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
