a9ccea91badb8c07030c8453187094c0cc4578e5
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:15:18 +0500

    Created cart delete by id command

---
+++ MarketPlace.Application/Carts/Commands/CartDeleteByIdCommand.cs
@@ -0,0 +1,8 @@
+﻿using MarketPlace.Domain.Common.Commands;
+
+namespace MarketPlace.Application.Carts.Commands;
+
+public record CartDeleteByIdCommand : ICommand<bool>
+{
+    public Guid CartId { get; set; }
+}
