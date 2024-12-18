d548c16c727f68af4490cdc6ef4dfb7dfbbe6e36
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:00:11 +0500

    Created notification aggregator interface

---
+++ MarketPlace.Application/Notifications/Services/INotificationAggregatorService.cs
@@ -0,0 +1,16 @@
+﻿using MarketPlace.Domain.Common.Exceptions;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Application.Notifications.Events;
+using MarketPlace.Application.Notifications.Models;
+
+namespace MarketPlace.Application.Notifications.Services;
+
+public interface INotificationAggregatorService
+{
+    public ValueTask<FuncResult<bool>> SendAsync(ProcessNotificationEvent processNotificationEvent, CancellationToken cancellationToken = default);
+
+    public ValueTask<IList<NotificationTemplate>> GetTemplatesByFilterAsync(
+        NotificationTemplateFilter notificationTemplateFilter,
+        CancellationToken cancellationToken = default
+        );
+}
