7373087c49a2c9dceeca4857773b159f4fdc494b
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:07:14 +0500

    Created cart item get by id query

---
+++ MarketPlace.Application/Carts/Queries/CartItemGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Application.Carts.Models;
+using MarketPlace.Domain.Common.Queries;
+
+namespace MarketPlace.Application.Carts.Queries;
+
+public record CartItemGetByIdQuery : IQuery<CartItemDto?>
+{
+    public Guid CartItemId { get; set; }
+}
