522a7c5c24cc26f99956d980962599e4d876d52a
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:05:58 +0500

    Created user get query

---
+++ MarketPlace.Application/Identity/Queries/UserGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Application.Identity.Models;
+
+namespace MarketPlace.Application.Identity.Queries;
+
+public class UserGetQuery : IQuery<ICollection<UserDto>>
+{
+    public UserFilter UserFilter { get; set; }
+}
