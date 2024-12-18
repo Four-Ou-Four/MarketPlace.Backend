12b6a5a8e564064616b08e6df995d1c3907e7ea6
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:05:54 +0500

    Created product configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(p => p.Name)
+               .IsRequired()
+               .HasMaxLength(100);
+
+        builder.HasMany(p => p.OrderItems)
+               .WithOne(oi => oi.Product)
+               .HasForeignKey(oi => oi.ProductId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(p => p.Category)
+            .WithMany(c => c.Products)
+            .HasForeignKey(p => p.CategoryId);
+
+
+    }
+}
