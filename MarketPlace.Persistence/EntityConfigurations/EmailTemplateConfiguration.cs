92dd1bfd4d742e4cce820b8445124ea38e897b9a
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:06:28 +0500

    Created notification template configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/EmailTemplateConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class EmailTemplateConfiguration : IEntityTypeConfiguration<EmailTemplate>
+{
+    public void Configure(EntityTypeBuilder<EmailTemplate> builder)
+    {
+        builder.Property(template => template.Subject).IsRequired().HasMaxLength(256);
+    }
+}
