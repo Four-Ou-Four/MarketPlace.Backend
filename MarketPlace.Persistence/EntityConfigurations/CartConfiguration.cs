3d7a1fe4ef8d7f20edc0b64c573c15909dcf9aaf
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:07:08 +0500

    Created cart configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/CartConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class CartConfiguration : IEntityTypeConfiguration<Cart>
+{
+    public void Configure(EntityTypeBuilder<Cart> builder)
+    {
+        builder.HasMany(c => c.CartItems)
+               .WithOne(ci => ci.Cart)
+               .HasForeignKey(ci => ci.CartId)
+               .OnDelete(DeleteBehavior.Cascade); throw new NotImplementedException();
+    }
+}
---
+++ MarketPlace.Persistence/EntityConfigurations/CartItemConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+{
+    public void Configure(EntityTypeBuilder<CartItem> builder)
+    {
+        builder.HasOne(ci => ci.Cart)
+            .WithMany(c => c.CartItems);
+    }
+}
