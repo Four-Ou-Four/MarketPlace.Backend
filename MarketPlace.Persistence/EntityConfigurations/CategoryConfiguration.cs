e8ae974fb2e7a836949dd9f2a73b62ce81ed1874
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:06:57 +0500

    Created category configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.Property(c => c.Name)
+               .IsRequired()
+               .HasMaxLength(100);
+
+        builder.HasMany(c => c.Products)
+            .WithOne(p => p.Category);
+    }
+}
