f06204fcffcd8839b811ed42328199289efbf0f9
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:13:33 +0500

    Created user update command

---
+++ MarketPlace.Application/Identity/Commands/UserUpdateCommand.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Application.Identity.Models;
+
+namespace MarketPlace.Application.Identity.Commands;
+
+public class UserUpdateCommand : ICommand<UserDto>
+{
+    public UserDto UserDto { get; set; }
+}
