68adbc8d8429829b183a8fcdef59601bac1178c5
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:59:55 +0500

    Created verification code interface

---
+++ MarketPlace.Application/Verification/Services/IVerificationCodeService.cs
@@ -0,0 +1,8 @@
+﻿using MarketPlace.Domain.Enums;
+
+namespace MarketPlace.Application.Verification.Services;
+
+public interface IVerificationCodeService
+{
+    ValueTask<VerificationType?> GetVerificationTypeAsync(string code, CancellationToken cancellationToken = default);
+}
