c4a957ebe813c0a650cbfdfb41404521dd91a5ab
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:15:05 +0500

    Created cart item create command

---
+++ MarketPlace.Application/Carts/Commands/CartItemCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Application.Carts.Models;
+using MarketPlace.Domain.Common.Commands;
+
+namespace MarketPlace.Application.Carts.Commands;
+
+public record CartItemCreateCommand : ICommand<CartItemDto>
+{
+    public CartItemDto CartItemDto { get; set; }
+}
