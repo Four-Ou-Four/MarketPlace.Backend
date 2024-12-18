f8cc7028f3625daabf2ee541ed5e45b58d0454d2
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:01:14 +0500

    Created user settings repository

---
+++ MarketPlace.Persistence/Repositories/Interfaces/IUserSettingsRepository.cs
@@ -0,0 +1,19 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Persistence.Repositories.Interfaces;
+
+public interface IUserSettingsRepository
+{
+    ValueTask<UserSettings?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<UserSettings> CreateAsync(
+        UserSettings userSettings,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+}
---
+++ MarketPlace.Persistence/Repositories/UserSettingsRepository.cs
@@ -0,0 +1,26 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Persistence.Caching.Brokers;
+using MarketPlace.Persistence.DataContexts;
+using MarketPlace.Persistence.Repositories.Interfaces;
+
+namespace MarketPlace.Persistence.Repositories;
+
+public class UserSettingsRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<UserSettings, AppDbContext>(appDbContext, cacheBroker),
+    IUserSettingsRepository
+
+{
+    public ValueTask<UserSettings?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<UserSettings> CreateAsync(
+        UserSettings userSettings,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(userSettings, commandOptions, cancellationToken);
+}
