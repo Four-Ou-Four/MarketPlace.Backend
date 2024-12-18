a8f2d572a059fc6330423751eef855ad57c1f6da
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:06:55 +0500

    Created cart get query

---
+++ MarketPlace.Application/Carts/Queries/CartGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Application.Carts.Models;
+
+namespace MarketPlace.Application.Carts.Queries;
+
+public record CartGetQuery : IQuery<ICollection<CartDto>>
+{
+    public CartFilter CartFilter { get; set; }
+}
