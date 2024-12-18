63e7223ff0d671417e31b51557be483e7b12e590
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:03:38 +0500

    Created access token generator interface

---
+++ MarketPlace.Application/Identity/Services/IAccessTokenGeneratorService.cs
@@ -0,0 +1,10 @@
+﻿using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Application.Identity.Services;
+
+public interface IAccessTokenGeneratorService
+{
+    AccessToken GetToken(User user);
+
+    Guid GetTokenId(string accessToken);
+}
