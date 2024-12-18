a95a2ead6d275372f7339f84d9570aaa1962155a
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:12:33 +0500

    Created notification event

---
+++ MarketPlace.Application/Notifications/Events/NotificationEvent.cs
@@ -0,0 +1,10 @@
+﻿using MarketPlace.Domain.Common.Events;
+
+namespace MarketPlace.Application.Notifications.Events;
+
+public record NotificationEvent : EventBase
+{
+    public Guid SenderUserId { get; set; }
+
+    public Guid ReceiverUserId { get; set; }
+}
