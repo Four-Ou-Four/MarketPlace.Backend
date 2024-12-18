622e55e09facfa615a03e3367d5762394794d16a
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:11:54 +0500

    Created send notification event

---
+++ MarketPlace.Application/Notifications/Events/SendNotificationEvent.cs
@@ -0,0 +1,8 @@
+﻿using MarketPlace.Application.Notifications.Models;
+
+namespace MarketPlace.Application.Notifications.Events;
+
+public record SendNotificationEvent : NotificationEvent
+{
+    public NotificationMessage Message { get; set; } = default!;
+}
