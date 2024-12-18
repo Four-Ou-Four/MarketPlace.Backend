ee33ed3ed5f884b33926c59a0c8928cfc7ac19d0
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:06:06 +0500

    Created order configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/OrderItemConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasOne(oi => oi.Order)
+            .WithMany(o => o.OrderItems);
+    }
+}
