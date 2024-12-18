ba7f6e366292d3ca8655b9e5231b02537f4ef7b8
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:29:30 +0500

    Created category queries

---
+++ MarketPlace.Application/Categories/Queries/CategoryGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Application.Categories.Models;
+
+namespace MarketPlace.Application.Categories.Queries;
+
+public record CategoryGetByIdQuery : IQuery<CategoryDto?>
+{
+    public Guid CategoryId { get; set; }
+}
---
+++ MarketPlace.Application/Categories/Queries/CategoryGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Application.Categories.Models;
+
+namespace MarketPlace.Application.Categories.Queries;
+
+public record CategoryGetQuery : IQuery<ICollection<CategoryDto>>
+{
+    public CategoryFilter CategoryFilter { get; set; }
+}
