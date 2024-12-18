409c40ee7b1ef6317df14329af3313fd1fd43f9a
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:05:11 +0500

    Created verification code configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/UserInfoVerificationCodeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class UserInfoVerificationCodeConfiguration : IEntityTypeConfiguration<UserInfoVerificationCode>
+{
+    public void Configure(EntityTypeBuilder<UserInfoVerificationCode> builder)
+    {
+        builder.HasOne<User>().WithMany().HasForeignKey(code => code.UserId);
+    }
+}
