96e2c8aaedcc30180e4a32724f4a57421be9743d
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:02:57 +0500

    Created email template repository

---
+++ MarketPlace.Persistence/Repositories/EmailTemplateRepository.cs
@@ -0,0 +1,43 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Persistence.Caching.Brokers;
+using MarketPlace.Persistence.DataContexts;
+using MarketPlace.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace MarketPlace.Persistence.Repositories;
+
+public class EmailTemplateRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<EmailTemplate, AppDbContext>(appDbContext, cacheBroker),
+    IEmailTemplateRepository
+
+{
+    public IQueryable<EmailTemplate> Get(
+        Expression<Func<EmailTemplate, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    base.Get(predicate, queryOptions);
+
+    public ValueTask<EmailTemplate?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<EmailTemplate>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    base.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<EmailTemplate> CreateAsync(
+        EmailTemplate emailTemplate,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(emailTemplate, commandOptions, cancellationToken);
+}
---
+++ MarketPlace.Persistence/Repositories/Interfaces/IEmailTemplateRepository.cs
@@ -0,0 +1,33 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MarketPlace.Persistence.Repositories.Interfaces;
+
+public interface IEmailTemplateRepository
+{
+    IQueryable<EmailTemplate> Get(
+             Expression<Func<EmailTemplate, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    ValueTask<EmailTemplate?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<EmailTemplate>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<EmailTemplate> CreateAsync(
+        EmailTemplate emailTemplate,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
+
