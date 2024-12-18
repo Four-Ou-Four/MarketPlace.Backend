28f82a3f1550c533eff08c87373253425264cb5e
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:16:14 +0500

    Created sign up/in details

---
+++ MarketPlace.Application/Identity/Models/SignInDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace MarketPlace.Application.Identity.Models;
+
+public class SignInDetails
+{
+    public string EmailAddress { get; set; } = default!;
+
+    public string Password { get; set; } = default!;
+}
