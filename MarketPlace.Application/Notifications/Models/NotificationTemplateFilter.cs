e60d64418ec90f2431748bf9d89d4e26b719784c
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:08:16 +0500

    Created notification template filter

---
+++ MarketPlace.Application/Notifications/Models/NotificationTemplateFilter.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Enums;
+using MarketPlace.Domain.Common.Queries;
+
+namespace MarketPlace.Application.Notifications.Models;
+
+public class NotificationTemplateFilter : FilterPagination
+{
+    public IList<NotificationType> TemplateType { get; set; }
+}
