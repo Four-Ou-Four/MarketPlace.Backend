9c6727154f94a3ae31fdeb41274c5997f493fe86
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:19:41 +0500

    Created event bus broker interface

---
+++ MarketPlace.Application/Common/EventBus/Brokers/IEventBusBroker.cs
@@ -0,0 +1,16 @@
+﻿using MarketPlace.Domain.Common.Events;
+
+namespace MarketPlace.Application.Common.EventBus.Brokers;
+
+public interface IEventBusBroker
+{
+    ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase;
+
+    ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase;
+}
