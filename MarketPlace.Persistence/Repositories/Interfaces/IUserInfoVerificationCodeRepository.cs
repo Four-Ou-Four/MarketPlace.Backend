77d00913dc8df60286146e43c495ae4dfc74a254
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:02:39 +0500

    User info verification code repository

---
+++ MarketPlace.Persistence/Repositories/Interfaces/IUserInfoVerificationCodeRepository.cs
@@ -0,0 +1,37 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MarketPlace.Persistence.Repositories.Interfaces;
+
+public interface IUserInfoVerificationCodeRepository
+{
+    IQueryable<UserInfoVerificationCode> Get(
+             Expression<Func<UserInfoVerificationCode, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    ValueTask<UserInfoVerificationCode?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<UserInfoVerificationCode>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<UserInfoVerificationCode> CreateAsync(
+        UserInfoVerificationCode userInfoVerificationCode,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask DeactivateAsync(
+        Guid codeId,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
