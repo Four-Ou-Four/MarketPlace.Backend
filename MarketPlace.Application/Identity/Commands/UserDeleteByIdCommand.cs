32adee76e25ce26dac4d75b0640169bdfcae1b98
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:13:49 +0500

    Created user delete by id command

---
+++ MarketPlace.Application/Identity/Commands/UserDeleteByIdCommand.cs
@@ -0,0 +1,8 @@
+﻿using MarketPlace.Domain.Common.Commands;
+
+namespace MarketPlace.Application.Identity.Commands;
+
+public class UserDeleteByIdCommand : ICommand<bool>
+{
+    public Guid UserId { get; set; }
+}
