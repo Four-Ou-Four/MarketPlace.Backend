25705b5aea32c731c398485f6f43b129ff58b37b
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:14:14 +0500

    Created cart update command

---
+++ MarketPlace.Application/Carts/Commands/CartUpdateCommand.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Application.Carts.Models;
+using MarketPlace.Domain.Common.Commands;
+
+namespace MarketPlace.Application.Carts.Commands;
+
+public record CartUpdateCommand : ICommand<CartDto>
+{
+    public CartDto CartDto { get; set; }
+}
