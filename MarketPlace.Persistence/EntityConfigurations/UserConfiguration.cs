9d45026b654e3e15b65f6cb3084f6e4cf4cbd050
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:05:44 +0500

    Created user configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using MarketPlace.Domain.Entities;
+using MarketPlace.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .Property(user => user.FirstName)
+            .HasMaxLength(64)
+            .IsRequired();
+        builder
+            .Property(user => user.LastName)
+            .HasMaxLength(64)
+            .IsRequired();
+        builder
+            .Property(user => user.EmailAddress)
+            .HasMaxLength(128)
+            .IsRequired();
+        builder
+            .Property(user => user.PasswordHash)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder
+            .Property(user => user.Role)
+            .HasConversion<string>()
+            .HasDefaultValue(Role.User)
+            .HasMaxLength(64)
+            .IsRequired();
+
+        builder
+            .HasIndex(user => user.EmailAddress)
+            .IsUnique();
+
+        builder.HasOne(u => u.Cart)
+               .WithOne(c => c.User)
+               .HasForeignKey<Cart>(c => c.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(u => u.Products)
+               .WithOne(p => p.User)
+               .HasForeignKey(p => p.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
