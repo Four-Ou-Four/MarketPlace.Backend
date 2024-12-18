4539dbeefafb23b15941b9eda907df698bcca9fb
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:02:12 +0500

    Created product repository

---
+++ MarketPlace.Persistence/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MarketPlace.Persistence.Repositories.Interfaces;
+
+public interface IProductRepository
+{
+    IQueryable<Product> Get(
+             Expression<Func<Product, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    ValueTask<Product?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<Product>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Product> CreateAsync(
+        Product product,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Product> UpdateAsync(
+        Product product,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Product?> DeleteAsync(
+        Product product,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Product?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
---
+++ MarketPlace.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Persistence.Caching.Brokers;
+using MarketPlace.Persistence.DataContexts;
+using MarketPlace.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace MarketPlace.Persistence.Repositories;
+
+public class ProductRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<Product, AppDbContext>(appDbContext, cacheBroker),
+    IProductRepository
+
+{
+    public IQueryable<Product> Get(
+        Expression<Func<Product, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    base.Get(predicate, queryOptions);
+
+    public ValueTask<Product?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<Product>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    base.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<Product> CreateAsync(
+        Product product,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(product, commandOptions, cancellationToken);
+
+    public ValueTask<Product> UpdateAsync(
+        Product product,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken) =>
+    base.UpdateAsync(product, commandOptions, cancellationToken);
+
+    public ValueTask<Product?> DeleteAsync(
+        Product product,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.UpdateAsync(product, commandOptions, cancellationToken);
+
+    public ValueTask<Product?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
