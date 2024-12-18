a3fbab325842074170af096ce7c2e84f6e2827b2
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:05:41 +0500

    Created email history get by id query

---
+++ MarketPlace.Application/Notifications/Queries/EmailHistoryGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Application.Notifications.Models;
+
+namespace MarketPlace.Application.Notifications.Queries;
+
+public class EmailHistoryGetByIdQuery : IQuery<EmailHistoryDto?>
+{
+    public Guid EmailHistoryId { get; set; }
+}
