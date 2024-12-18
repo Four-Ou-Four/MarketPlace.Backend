5c10bb740ff8c5f222126e16f5eef46f7bd6e046
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:04:24 +0500

    Created access token repository

---
+++ MarketPlace.Persistence/Repositories/Interfaces/IAccessTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Persistence.Repositories.Interfaces;
+
+public interface IAccessTokenRepository
+{
+    ValueTask<AccessToken> CreateAsync(
+        AccessToken accessToken,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default);
+    ValueTask<AccessToken?> GetByIdAsync(
+        Guid accessTokeId,
+        CancellationToken cancellationToken = default);
+    ValueTask<AccessToken> UpdateAsync(
+        AccessToken accessToken,
+        CancellationToken cancellationToken = default);
+}
