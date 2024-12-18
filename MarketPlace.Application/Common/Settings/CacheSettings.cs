6ef77c82b740bd4dbc046ed063a8ccca8e9135f8
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:19:27 +0500

    Created cache settings

---
+++ MarketPlace.Application/Common/Settings/CacheSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MarketPlace.Application.Common.Settings;
+
+public class CacheSettings
+{
+    public int AbsoluteExpirationInSeconds { get; set; }
+
+    public int SlidingExpirationInSeconds { get; set; }
+}
