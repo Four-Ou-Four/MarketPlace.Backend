e3860b65e645503802ea6bd887be93f2964de44b
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:11:44 +0500

    Created email process notification event

---
+++ MarketPlace.Application/Notifications/Models/EmailProcessNotificationEvent.cs
@@ -0,0 +1,12 @@
+﻿using MarketPlace.Domain.Enums;
+using MarketPlace.Application.Notifications.Events;
+
+namespace MarketPlace.Application.Notifications.Models;
+
+public record EmailProcessNotificationEvent : ProcessNotificationEvent
+{
+    public EmailProcessNotificationEvent()
+    {
+        Type = NotificationType.Email;
+    }
+}
