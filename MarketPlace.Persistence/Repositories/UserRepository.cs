a52bf4a8bfa0fa1c9e9c27861146cdfc52fd8cae
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 14:01:53 +0500

    Created user repository

---
+++ MarketPlace.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,61 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Persistence.Caching.Brokers;
+using MarketPlace.Persistence.DataContexts;
+using MarketPlace.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace MarketPlace.Persistence.Repositories;
+
+public class UserRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<User, AppDbContext>(appDbContext, cacheBroker),
+    IUserRepository
+
+{
+    public IQueryable<User> Get(
+        Expression<Func<User, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    base.Get(predicate, queryOptions);
+
+    public ValueTask<User?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<User>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    base.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<User> CreateAsync(
+        User user,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(user, commandOptions, cancellationToken);
+
+    public ValueTask<User> UpdateAsync(
+        User user,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken) =>
+    base.UpdateAsync(user, commandOptions, cancellationToken);
+
+    public ValueTask<User?> DeleteAsync(
+        User user,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.UpdateAsync(user, commandOptions, cancellationToken);
+
+    public ValueTask<User?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
