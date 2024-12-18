4fc5a59b2bddac9ffb7a9bc3f7edd7b850901671
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:13:19 +0500

    Created email template create command

---
+++ MarketPlace.Application/Notifications/Commands/EmailTemplateCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Application.Notifications.Models;
+
+namespace MarketPlace.Application.Notifications.Commands;
+
+public class EmailTemplateCreateCommand : ICommand<EmailTemplateDto>
+{
+    public EmailTemplateDto EmailTemplateDto { get; set; }
+}
