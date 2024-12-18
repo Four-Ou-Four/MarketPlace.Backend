e60133bd105561df06f7e4fafc1f54b7ce8013b0
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:05:00 +0500

    Created email history get query

---
+++ MarketPlace.Application/Notifications/Queries/EmailHistoryGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Application.Notifications.Models;
+
+namespace MarketPlace.Application.Notifications.Queries;
+
+public class EmailHistoryGetQuery : IQuery<ICollection<EmailHistoryDto>>
+{
+    public EmailHistoryFilter EmailHistoryFilter { get; set; }
+}
