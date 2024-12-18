589cc8e79ac7b3ad080a701669f4091e3aa9ba82
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-18 15:01:05 +0500

    Created email orchestration interface

---
+++ MarketPlace.Application/Notifications/Services/IEmailOrchestrationService.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Domain.Common.Exceptions;
+using MarketPlace.Application.Notifications.Models;
+
+namespace MarketPlace.Application.Notifications.Services;
+
+public interface IEmailOrchestrationService
+{
+    ValueTask<FuncResult<bool>> SendAsync(EmailProcessNotificationEvent @event, CancellationToken cancellationToken = default);
+}
