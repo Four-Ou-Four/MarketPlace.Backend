2dcda113810be46781d32be3e54bce59593bf79a
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 17:32:59 +0500

    Created product filter

---
+++ MarketPlace.Application/Products/Models/ProductFilter.cs
@@ -0,0 +1,29 @@
+﻿using MarketPlace.Domain.Common.Queries;
+
+namespace MarketPlace.Application.Answers.Models;
+
+public class ProductFilter : FilterPagination
+{
+    /// <summary>
+    /// Overrides base GetHashCode method
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    /// <summary>
+    /// Overrides base Equels method
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object? obj) =>
+        obj is ProductFilter filter
+        && filter.GetHashCode() == GetHashCode();
+}
