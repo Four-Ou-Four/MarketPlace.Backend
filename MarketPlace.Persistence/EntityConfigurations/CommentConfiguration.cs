08078914995d6791708ce852262b7d5a364ad3f7
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 18:51:53 +0500

    Created comment entity configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/CommentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.HasOne(c => c.ParentComment)
+               .WithMany(c => c.Replies)
+               .HasForeignKey(c => c.ParentCommentId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(c => c.User)
+               .WithMany(u => u.Comments)
+               .HasForeignKey(c => c.UserId);
+
+        builder.HasOne(c => c.Product)
+               .WithMany(p => p.Comments)
+               .HasForeignKey(c => c.ProductId);
+    }
+}
