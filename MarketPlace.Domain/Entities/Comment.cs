07c18d2b782b01678a9d77aacbb7f482818e5448
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 18:52:52 +0500

    Created comment entity

---
+++ MarketPlace.Domain/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿using MarketPlace.Domain.Common.Entities;
+
+namespace MarketPlace.Domain.Entities;
+
+public class Comment : AuditableEntity
+{
+    public Guid UserId { get; set; }
+    public Guid ProductId { get; set; }
+    public string Content { get; set; }
+    public Guid? ParentCommentId { get; set; }
+    public User User { get; set; }
+    public Product Product { get; set; }
+    public Comment ParentComment { get; set; }
+    public IEnumerable<Comment> Replies { get; set; }
+}
