5aa3b18626d5b5f21c589b42ad1c6e1c6c062f26
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 17:32:49 +0500

    Created product queries

---
+++ MarketPlace.Application/Products/Queries/ProductGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Application.Answers.Models;
+using MarketPlace.Domain.Common.Queries;
+
+namespace MarketPlace.Application.Answers.Queries;
+
+public class ProductGetByIdQuery : IQuery<ProductDto?>
+{
+    public Guid ProductId { get; set; }
+}
---
+++ MarketPlace.Application/Products/Queries/ProductGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Application.Answers.Models;
+using MarketPlace.Domain.Common.Queries;
+
+namespace MarketPlace.Application.Answers.Queries;
+
+public class ProductGetQuery : IQuery<ICollection<ProductDto>>
+{
+    public ProductFilter ProductFilter { get; set; }
+}
