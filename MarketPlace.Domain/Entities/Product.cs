afed2d4890d59f42b987a5108fe93d0e3727787c
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 18:52:42 +0500

    Edited product entity

--- MarketPlace.Domain/Entities/Product.cs
+++ MarketPlace.Domain/Entities/Product.cs
@@ -9,7 +9,11 @@ public class Product : AuditableEntity
     public string Description { get; set; }
     public Guid UserId { get; set; }
     public Guid CategoryId { get; set; }
+    public float Rating { get; set; }
     public User User { get; set; }
     public Category Category { get; set; }
     public IEnumerable<OrderItem> OrderItems { get; set; }
+    public IEnumerable<CartItem> CartItems { get; set; }
+    public IEnumerable<Comment> Comments { get; set; }
+    public IEnumerable<Rating> Ratings { get; set; }
 }
