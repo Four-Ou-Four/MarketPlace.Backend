0c1a27b38f478407a424086c9bddd18cbb52fd91
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 18:51:18 +0500

    Created rating entity configuration

---
+++ MarketPlace.Persistence/EntityConfigurations/RatingConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using MarketPlace.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MarketPlace.Persistence.EntityConfigurations;
+
+public class RatingConfiguration : IEntityTypeConfiguration<Rating>
+{
+    public void Configure(EntityTypeBuilder<Rating> builder)
+    {
+        builder.Property(r => r.Stars)
+               .IsRequired();
+
+        builder.HasOne(r => r.User)
+               .WithMany(u => u.Ratings)
+               .HasForeignKey(r => r.UserId);
+
+        builder.HasOne(r => r.Product)
+               .WithMany(p => p.Ratings)
+               .HasForeignKey(r => r.ProductId);
+    }
+}
