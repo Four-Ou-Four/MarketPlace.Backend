56b2ef1be6aabe46a11560040474eba03c779d60
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 17:32:11 +0500

    Edited order item entity

--- MarketPlace.Domain/Entities/OrderItem.cs
+++ MarketPlace.Domain/Entities/OrderItem.cs
@@ -5,9 +5,9 @@ namespace MarketPlace.Domain.Entities;
 public class OrderItem : AuditableEntity
 {
     public Guid OrderId { get; set; }
-    public Order Order { get; set; }
     public Guid ProductId { get; set; }
-    public Product Product { get; set; }
     public int Quantity { get; set; }
     public decimal TotalPrice { get; set; }
+    public Order Order { get; set; }
+    public Product Product { get; set; }
 }
