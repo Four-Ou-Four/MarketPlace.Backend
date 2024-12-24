8ef83e3e06c3c55ffc5f4a751b426c5dbb22dcac
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 18:52:33 +0500

    Created rating entity

---
+++ MarketPlace.Domain/Entities/Rating.cs
@@ -0,0 +1,12 @@
+﻿using MarketPlace.Domain.Common.Entities;
+
+namespace MarketPlace.Domain.Entities;
+
+public class Rating : AuditableEntity
+{
+    public Guid UserId { get; set; }
+    public Guid ProductId { get; set; }
+    public int Stars { get; set; }
+    public User User { get; set; }
+    public Product Product { get; set; }
+}
