b3bf6db9026d2abd0d76cef624b2b11230199feb
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 17:33:43 +0500

    Created order service interface

---
+++ MarketPlace.Application/Orders/Services/IOrderItemService.cs
@@ -0,0 +1,52 @@
+﻿using MarketPlace.Application.Orders.Models;
+using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace MarketPlace.Application.Orders.Services;
+
+public interface IOrderItemService
+{
+    IQueryable<OrderItem> Get(
+             Expression<Func<OrderItem, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    IQueryable<OrderItem> Get(
+        OrderItemFilter orderItemFilter,
+        QueryOptions queryOptions = default);
+
+    ValueTask<OrderItem?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<OrderItem>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<OrderItem> CreateAsync(
+        OrderItem orderItem,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<OrderItem> UpdateAsync(
+        OrderItem orderItem,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<OrderItem?> DeleteAsync(
+        OrderItem orderItem,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<OrderItem?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
---
+++ MarketPlace.Application/Orders/Services/IOrderService.cs
@@ -0,0 +1,52 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Application.Orders.Models;
+using System.Linq.Expressions;
+
+namespace MarketPlace.Application.Orders.Services;
+
+public interface IOrderService
+{
+    IQueryable<Order> Get(
+             Expression<Func<Order, bool>>? predicate = default,
+             QueryOptions queryOptions = default);
+
+    IQueryable<Order> Get(
+        OrderFilter orderFilter,
+        QueryOptions queryOptions = default);
+
+    ValueTask<Order?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<IList<Order>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Order> CreateAsync(
+        Order order,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Order> UpdateAsync(
+        Order order,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Order?> DeleteAsync(
+        Order order,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Order?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+}
