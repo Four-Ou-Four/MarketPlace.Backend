bf825a4e3f1d94e042566278b3f47d84462cd734
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 18:51:02 +0500

    Created comment repository

---
+++ MarketPlace.Persistence/Repositories/CommentRepository.cs
@@ -0,0 +1,61 @@
+﻿using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Persistence.Caching.Brokers;
+using MarketPlace.Persistence.DataContexts;
+using MarketPlace.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace MarketPlace.Persistence.Repositories;
+
+public class CommentRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<Comment, AppDbContext>(appDbContext, cacheBroker),
+    ICommentRepository
+
+{
+    public IQueryable<Comment> Get(
+        Expression<Func<Comment, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    base.Get(predicate, queryOptions);
+
+    public ValueTask<Comment?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<Comment>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    base.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<Comment> CreateAsync(
+        Comment comment,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(comment, commandOptions, cancellationToken);
+
+    public ValueTask<Comment> UpdateAsync(
+        Comment comment,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken) =>
+    base.UpdateAsync(comment, commandOptions, cancellationToken);
+
+    public ValueTask<Comment?> DeleteAsync(
+        Comment comment,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.UpdateAsync(comment, commandOptions, cancellationToken);
+
+    public ValueTask<Comment?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
