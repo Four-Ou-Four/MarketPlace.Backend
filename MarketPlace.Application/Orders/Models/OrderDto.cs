f11e53c8f946feb218658363c5fc8ed5324bcb8f
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 17:34:42 +0500

    Created order dtos

---
+++ MarketPlace.Application/Orders/Models/OrderDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MarketPlace.Application.Orders.Models;
+
+public class OrderDto
+{
+    public Guid? Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public Guid UserId { get; set; }
+}
---
+++ MarketPlace.Application/Orders/Models/OrderItemDto.cs
@@ -0,0 +1,12 @@
+﻿using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Application.Orders.Models;
+
+public class OrderItemDto
+{
+    public Guid? Id { get; set; }
+    public Guid OrderId { get; set; }
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal TotalPrice { get; set; }
+}
