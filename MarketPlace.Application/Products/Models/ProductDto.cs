eb72bde2befae2af375704d3b00d858c4838cf86
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 17:33:10 +0500

    Created product dto

---
+++ MarketPlace.Application/Products/Models/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MarketPlace.Application.Answers.Models;
+
+public class ProductDto
+{
+    public Guid? Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+    public Guid UserId { get; set; }
+    public Guid CategoryId { get; set; }
+}
