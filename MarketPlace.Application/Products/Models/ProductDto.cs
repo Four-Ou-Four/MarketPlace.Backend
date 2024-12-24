694c7e453dcc92e3d60d1e34593b0db8353f61c5
Habiba Sattorova <habibasattorova26@gmail.com>
2024-12-24 18:53:08 +0500

    Edited product dto

--- MarketPlace.Application/Products/Models/ProductDto.cs
+++ MarketPlace.Application/Products/Models/ProductDto.cs
@@ -8,4 +8,5 @@ public class ProductDto
     public string Description { get; set; }
     public Guid UserId { get; set; }
     public Guid CategoryId { get; set; }
+    public float Rating { get; set; }
 }
