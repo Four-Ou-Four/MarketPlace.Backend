43b32d723a3aabe0715d7dc9cbdc793afcbfe240
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 22:22:35 +0500

    Created category validator

---
+++ MarketPlace.Infrastructure/Categories/Validators/CategoryValidator.cs
@@ -0,0 +1,11 @@
+﻿using MarketPlace.Domain.Entities;
+using FluentValidation;
+
+namespace MarketPlace.Infrastructure.Categories.Validators;
+public class CategoryValidator : AbstractValidator<Category>
+{
+    public CategoryValidator()
+    {
+        RuleFor(category => category.Name).NotEmpty().MinimumLength(2);
+    }
+}
