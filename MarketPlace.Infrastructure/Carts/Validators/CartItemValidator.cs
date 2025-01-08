754a0863b58ec7439c17c3aa470ec3803481d9e7
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 17:00:59 +0500

    Created cart item validator

---
+++ MarketPlace.Infrastructure/Carts/Validators/CartItemValidator.cs
@@ -0,0 +1,27 @@
+﻿using MarketPlace.Domain.Entities;
+using MarketPlace.Domain.Enums;
+using FluentValidation;
+
+namespace MarketPlace.Infrastructure.Carts.Validators;
+public class CartItemValidator : AbstractValidator<CartItem>
+{
+    public CartItemValidator()
+    {
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(cartItem => cartItem.CartId).NotEqual(Guid.Empty);
+                RuleFor(cartItem => cartItem.ProductId).NotEqual(Guid.Empty);
+                RuleFor(cartItem => cartItem.Quantity).NotEmpty().GreaterThanOrEqualTo(1);
+            });
+
+        RuleSet(
+            EntityEvent.OnUpdate.ToString(),
+            () =>
+            {
+                RuleFor(cartItem => cartItem.ProductId).NotEqual(Guid.Empty);
+                RuleFor(cartItem => cartItem.Quantity).NotEmpty().GreaterThanOrEqualTo(1);
+            });
+    }
+}
