62c7a99c9e9efce02a042b2b6e616b1398b046b2
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 12:00:05 +0500

    Implemented account aggregator service

---
+++ MarketPlace.Infrastructure/Identity/Services/AccountAggregatorService.cs
@@ -0,0 +1,72 @@
+﻿using MarketPlace.Application.Common.EventBus.Brokers;
+using MarketPlace.Application.Identity.Services;
+using MarketPlace.Application.Notifications.Events;
+using MarketPlace.Application.Verification.Services;
+using MarketPlace.Domain.Constants;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Domain.Enums;
+
+namespace MarketPlace.Infrastructure.Identity.Services;
+
+public class AccountAggregatorService(
+    IUserService userService,
+    IUserSettingsService userSettingsService,
+    IUserInfoVerificationCodeService userInfoVerificationCodeService,
+    IEventBusBroker eventBusBroker
+) : IAccountAggregatorService
+{
+    public async ValueTask<bool> CreateUserAsync(User user, CancellationToken cancellationToken = default)
+    {
+        user.Role = Role.User;
+        var createdUser = await userService.CreateAsync(user, cancellationToken: cancellationToken);
+        await userSettingsService.CreateAsync(
+            new UserSettings
+            {
+                Id = createdUser.Id,
+            },
+            cancellationToken: cancellationToken
+        );
+
+        var welcomeNotificationEvent = new ProcessNotificationEvent
+        {
+            ReceiverUserId = createdUser.Id,
+            TemplateType = NotificationTemplateType.WelcomeNotification,
+            Variables = new Dictionary<string, string>
+            {
+                { NotificationTemplateConstants.UserNamePlaceholder, createdUser.FirstName}
+            }
+        };
+
+        await eventBusBroker.PublishAsync(
+            welcomeNotificationEvent,
+            //EventBusConstants.NotificationExchangeName,
+            //EventBusConstants.ProcessNotificationQueueName,
+            cancellationToken
+        );
+
+        var verificationCode = await userInfoVerificationCodeService.CreateAsync(
+            VerificationCodeType.EmailAddressVerification,
+            createdUser.Id,
+            cancellationToken
+        );
+
+        var senderVerificationEvent = new ProcessNotificationEvent
+        {
+            ReceiverUserId = createdUser.Id,
+            TemplateType = NotificationTemplateType.EmailAddressVerificationNotification,
+            Variables = new Dictionary<string, string>
+            {
+                { NotificationTemplateConstants.EmailAddressVerificationLinkPlaceholder, verificationCode.VerificationLink}
+            }
+        };
+
+        await eventBusBroker.PublishAsync(
+            senderVerificationEvent,
+            //EventBusConstants.NotificationExchangeName,
+            //EventBusConstants.ProcessNotificationQueueName,
+            cancellationToken
+        );
+
+        return true;
+    }
+}
