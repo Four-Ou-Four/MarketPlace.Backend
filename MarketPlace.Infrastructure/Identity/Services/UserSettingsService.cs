27d8ec114fc75761333d15f8b8872d457f483f91
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 11:58:57 +0500

    Implemented user settings service

---
+++ MarketPlace.Infrastructure/Identity/Services/UserSettingsService.cs
@@ -0,0 +1,22 @@
+﻿using MarketPlace.Application.Identity.Services;
+using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Common.Queries;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Persistence.Repositories.Interfaces;
+
+namespace MarketPlace.Infrastructure.Identity.Services;
+
+public class UserSettingsService(IUserSettingsRepository userSettingsRepository) : IUserSettingsService
+{
+    public ValueTask<UserSettings?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+    userSettingsRepository.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<UserSettings> CreateAsync(
+        UserSettings userSettings,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default) =>
+    userSettingsRepository.CreateAsync(userSettings, commandOptions, cancellationToken);
+}
