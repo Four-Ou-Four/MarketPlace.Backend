21a8e77232ff4909f73dbbb8c4b6815730eb6c57
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 12:01:02 +0500

    Created user mapper

---
+++ MarketPlace.Infrastructure/Identity/Mappers/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Identity.Models;
+using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Infrastructure.Identity.Mappers;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<User, UserDto>().ReverseMap();
+        CreateMap<SignInDetails, User>().ReverseMap();
+        CreateMap<SignUpDetails, User>().ReverseMap();
+    }
+}
