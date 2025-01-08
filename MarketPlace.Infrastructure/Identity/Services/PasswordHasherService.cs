75b88249a2613628c530244b6ebe5934da0088ae
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 11:59:20 +0500

    Implemented password hasher service

---
+++ MarketPlace.Infrastructure/Identity/Services/PasswordHasherService.cs
@@ -0,0 +1,17 @@
+﻿using MarketPlace.Application.Identity.Services;
+using BC = BCrypt.Net.BCrypt;
+
+namespace MarketPlace.Infrastructure.Identity.Services;
+
+public class PasswordHasherService : IPasswordHasherService
+{
+    public string HashPassword(string password)
+    {
+        return BC.HashPassword(password);
+    }
+
+    public bool ValidatePassword(string password, string hashedPassword)
+    {
+        return BC.Verify(password, hashedPassword);
+    }
+}
