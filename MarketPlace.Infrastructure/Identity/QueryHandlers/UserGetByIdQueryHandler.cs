60503a9b86dffe34b4445c5890e2b269a37ef439
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 12:00:52 +0500

    Created user query handlers

---
+++ MarketPlace.Infrastructure/Identity/QueryHandlers/UserGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Identity.Models;
+using MarketPlace.Application.Identity.Queries;
+using MarketPlace.Application.Identity.Services;
+using MarketPlace.Domain.Common.Queries;
+
+namespace MarketPlace.Infrastructure.Identity.QueryHandlers;
+
+public class UserGetByIdQueryHandler(
+    IMapper mapper,
+    IUserService userService)
+    : IQueryHandler<UserGetByIdQuery, UserDto>
+{
+    public async Task<UserDto> Handle(UserGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await userService.GetByIdAsync(request.UserId, cancellationToken: cancellationToken);
+
+        return mapper.Map<UserDto>(result);
+    }
+}
---
+++ MarketPlace.Infrastructure/Identity/QueryHandlers/UserGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Identity.Models;
+using MarketPlace.Application.Identity.Queries;
+using MarketPlace.Application.Identity.Services;
+using MarketPlace.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Infrastructure.Identity.QueryHandlers;
+
+public class UserGetQueryHandler(
+    IMapper mapper,
+    IUserService userService)
+    : IQueryHandler<UserGetQuery, ICollection<UserDto>>
+{
+    public async Task<ICollection<UserDto>> Handle(UserGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await userService.Get(
+            request.UserFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<UserDto>>(result);
+    }
+}
