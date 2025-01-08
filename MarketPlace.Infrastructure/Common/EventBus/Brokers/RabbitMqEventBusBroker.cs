8eb837e906a006a8b075270af1e924b51a0eb71b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 12:02:20 +0500

    Implemented rabbit mq event bus broker

---
+++ MarketPlace.Infrastructure/Common/EventBus/Brokers/RabbitMqEventBusBroker.cs
@@ -0,0 +1,20 @@
+﻿using MarketPlace.Application.Common.EventBus.Brokers;
+using MarketPlace.Domain.Common.Events;
+using MediatR;
+
+namespace MarketPlace.Infrastructure.Common.EventBus.Brokers;
+
+public class RabbitMqEventBusBroker(IPublisher publisher) : IEventBusBroker
+{
+    public async ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+    await publisher.Publish(@event, cancellationToken);
+
+    public async ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+    await publisher.Publish(@event, cancellationToken);
+}
