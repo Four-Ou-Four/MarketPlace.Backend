13e4bda8f8db10f5602fb846d9b5c2c55fdff660
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 22:23:14 +0500

    Created category mapper

---
+++ MarketPlace.Infrastructure/Categories/Mappers/CategoryMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Categories.Models;
+using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Infrastructure.Categories.Mappers;
+
+public class CategoryMapper : Profile
+{
+    public CategoryMapper()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+    }
+}
