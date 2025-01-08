e3ad2ffce4f7b966c616e560604d85be9505dbf5
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 17:08:09 +0500

    Created cart mapper

---
+++ MarketPlace.Infrastructure/Carts/Mappers/CartMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Carts.Models;
+using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Infrastructure.Carts.Mappers;
+
+public class CartMapper : Profile
+{
+    public CartMapper()
+    {
+        CreateMap<Cart, CartDto>().ReverseMap();
+        CreateMap<CartItem, CartItemDto>().ReverseMap();
+    }
+}
