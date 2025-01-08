25acc4a7c6a062b2df7503a6d5af869e4fb635ff
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 17:00:38 +0500

    Created cart validator

---
+++ MarketPlace.Infrastructure/Carts/Validators/CartValidator.cs
@@ -0,0 +1,11 @@
+﻿using MarketPlace.Domain.Entities;
+using FluentValidation;
+
+namespace MarketPlace.Infrastructure.Carts.Validators;
+public class CartValidator : AbstractValidator<Cart>
+{
+    public CartValidator()
+    {
+        RuleFor(cart => cart.UserId).NotEqual(Guid.Empty);
+    }
+}
