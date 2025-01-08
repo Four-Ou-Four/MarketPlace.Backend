5c6a876b58b3e905e2c049316d3a4b87302c3bd3
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 11:54:29 +0500

    Created verification validator

---
+++ MarketPlace.Infrastructure/Verification/Validators/UserInfoVerificationCodeValidator.cs
@@ -0,0 +1,32 @@
+﻿using MarketPlace.Application.Common.Settings;
+using MarketPlace.Domain.Entities;
+using MarketPlace.Domain.Enums;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace MarketPlace.Infrastructure.Verification.Validators;
+
+public class UserInfoVerificationCodeValidator : AbstractValidator<UserInfoVerificationCode>
+{
+    public UserInfoVerificationCodeValidator(IOptions<VerificationSettings> verificationSettings, IOptions<ValidationSettings> validationSettings)
+    {
+        var verificationSettingsValue = verificationSettings.Value;
+        var validationSettingsValue = validationSettings.Value;
+
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(code => code.UserId).NotEqual(Guid.Empty);
+
+                RuleFor(code => code.ExpiryTime)
+                    .GreaterThanOrEqualTo(DateTimeOffset.UtcNow)
+                    .LessThanOrEqualTo(DateTimeOffset.UtcNow.AddSeconds(verificationSettingsValue.VerificationCodeExpiryTimeInSeconds));
+
+                RuleFor(code => code.IsActive).Equal(true);
+
+                RuleFor(code => code.VerificationLink).NotEmpty().Matches(validationSettingsValue.UrlRegexPattern);
+            }
+        );
+    }
+}
