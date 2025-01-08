434b81926f42f33bba152d10c8bfec83649ccdd5
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 17:02:18 +0500

    Created cart query handlers

---
+++ MarketPlace.Infrastructure/Carts/QueryHandlers/CartGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Carts.Models;
+using MarketPlace.Application.Carts.Queries;
+using MarketPlace.Application.Carts.Services;
+using MarketPlace.Domain.Common.Queries;
+
+namespace MarketPlace.Infrastructure.Carts.QueryHandlers;
+
+public class CartGetByIdQueryHandler(
+    IMapper mapper,
+    ICartService cartService)
+    : IQueryHandler<CartGetByIdQuery, CartDto>
+{
+    public async Task<CartDto> Handle(CartGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await cartService.GetByIdAsync(request.CartId, cancellationToken: cancellationToken);
+
+        return mapper.Map<CartDto>(result);
+    }
+}
---
+++ MarketPlace.Infrastructure/Carts/QueryHandlers/CartGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Carts.Models;
+using MarketPlace.Application.Carts.Queries;
+using MarketPlace.Application.Carts.Services;
+using MarketPlace.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Infrastructure.Carts.QueryHandlers;
+
+public class CartGetQueryHandler(
+    IMapper mapper,
+    ICartService cartService)
+    : IQueryHandler<CartGetQuery, ICollection<CartDto>>
+{
+    public async Task<ICollection<CartDto>> Handle(CartGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await cartService.Get(
+            request.CartFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<CartDto>>(result);
+    }
+}
