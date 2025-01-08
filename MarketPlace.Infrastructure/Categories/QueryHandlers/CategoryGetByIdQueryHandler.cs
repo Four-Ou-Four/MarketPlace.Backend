4615b67634af51c7e3147e280610043071019e9c
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 22:23:03 +0500

    Created category query handlers

---
+++ MarketPlace.Infrastructure/Categories/QueryHandlers/CategoryGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Categories.Models;
+using MarketPlace.Application.Categories.Queries;
+using MarketPlace.Application.Categories.Services;
+using MarketPlace.Domain.Common.Queries;
+
+namespace MarketPlace.Infrastructure.Categories.QueryHandlers;
+
+public class CategoryGetByIdQueryHandler(
+    IMapper mapper,
+    ICategoryService categoryService)
+    : IQueryHandler<CategoryGetByIdQuery, CategoryDto>
+{
+    public async Task<CategoryDto> Handle(CategoryGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await categoryService.GetByIdAsync(request.CategoryId, cancellationToken: cancellationToken);
+
+        return mapper.Map<CategoryDto>(result);
+    }
+}
---
+++ MarketPlace.Infrastructure/Categories/QueryHandlers/CategoryGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Categories.Models;
+using MarketPlace.Application.Categories.Queries;
+using MarketPlace.Application.Categories.Services;
+using MarketPlace.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Infrastructure.Categories.QueryHandlers;
+
+public class CategoryGetQueryHandler(
+    IMapper mapper,
+    ICategoryService categoryService)
+    : IQueryHandler<CategoryGetQuery, ICollection<CategoryDto>>
+{
+    public async Task<ICollection<CategoryDto>> Handle(CategoryGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await categoryService.Get(
+            request.CategoryFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<CategoryDto>>(result);
+    }
+}
