2c78fec3d1561b7b712be824684cf56b668e844d
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 12:02:36 +0500

    Implemented mass transit event bus broker

---
+++ MarketPlace.Infrastructure/Common/EventBus/Brokers/MassTransitEventBusBroker.cs
@@ -0,0 +1,21 @@
+﻿using MarketPlace.Application.Common.EventBus.Brokers;
+using MarketPlace.Domain.Common.Events;
+using MassTransit;
+using MediatR;
+
+namespace MarketPlace.Infrastructure.Common.EventBus.Brokers;
+
+public class MassTransitEventBusBroker(IBus bus, IPublisher publisher) : IEventBusBroker
+{
+    public async ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+        await publisher.Publish(@event, cancellationToken);
+
+    public async ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+        await bus.Publish(@event, cancellationToken);
+}
