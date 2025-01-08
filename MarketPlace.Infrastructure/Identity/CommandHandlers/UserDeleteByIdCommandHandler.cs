6ff9c0d537b2cb055a81ca7663c1fc37470c3455
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 12:01:21 +0500

    Created user command handlers

---
+++ MarketPlace.Infrastructure/Identity/CommandHandlers/UserDeleteByIdCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MarketPlace.Application.Identity.Commands;
+using MarketPlace.Application.Identity.Services;
+using MarketPlace.Domain.Common.Commands;
+
+namespace MarketPlace.Infrastructure.Identity.CommandHandlers;
+
+public class UserDeleteByIdCommandHandler(
+    IUserService userService)
+    : ICommandHandler<UserDeleteByIdCommand, bool>
+{
+    public async Task<bool> Handle(UserDeleteByIdCommand request, CancellationToken cancellationToken)
+    {
+        var result = await userService.DeleteByIdAsync(request.UserId, cancellationToken: cancellationToken);
+
+        return result is not null;
+    }
+}
---
+++ MarketPlace.Infrastructure/Identity/CommandHandlers/UserUpdateCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Identity.Commands;
+using MarketPlace.Application.Identity.Models;
+using MarketPlace.Application.Identity.Services;
+using MarketPlace.Domain.Common.Commands;
+using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Infrastructure.Identity.CommandHandlers;
+
+public class UserUpdateCommandHandler(
+    IMapper mapper,
+    IUserService userService) : ICommandHandler<UserUpdateCommand, UserDto>
+{
+    public async Task<UserDto> Handle(UserUpdateCommand request, CancellationToken cancellationToken)
+    {
+        var user = mapper.Map<User>(request.UserDto);
+
+        var updatedUser = await userService.UpdateAsync(user, cancellationToken: cancellationToken);
+
+        return mapper.Map<UserDto>(updatedUser);
+    }
+}
