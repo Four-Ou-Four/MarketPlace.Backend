24e2ae0dfe7e2c4acc82fd339971e498eba0f00e
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-08 11:58:43 +0500

    Created identity validators

---
+++ MarketPlace.Infrastructure/Identity/Validators/SignInDetailsValidator.cs
@@ -0,0 +1,18 @@
+﻿using MarketPlace.Application.Common.Settings;
+using MarketPlace.Application.Identity.Models;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace MarketPlace.Infrastructure.Identity.Validators;
+
+public class SignInDetailsValidator : AbstractValidator<SignInDetails>
+{
+    public SignInDetailsValidator(IOptions<ValidationSettings> validationSettings)
+    {
+        var validationSettingsValue = validationSettings.Value;
+
+        RuleFor(x => x.EmailAddress).NotEmpty().Matches(validationSettingsValue.EmailAddressRegexPattern);
+
+        RuleFor(x => x.Password).NotEmpty();
+    }
+}
