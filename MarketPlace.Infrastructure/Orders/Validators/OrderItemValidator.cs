aac2d2012c0da77ad55dfe9dcae749197100e58f
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-17 19:40:08 +0500

    Created order item validator

---
+++ MarketPlace.Infrastructure/Orders/Validators/OrderItemValidator.cs
@@ -0,0 +1,28 @@
+﻿using MarketPlace.Domain.Entities;
+using MarketPlace.Domain.Enums;
+using FluentValidation;
+
+namespace MarketPlace.Infrastructure.Orders.Validators;
+public class OrderItemValidator : AbstractValidator<OrderItem>
+{
+    public OrderItemValidator()
+    {
+        RuleSet(
+            EntityEvent.OnCreate.ToString(),
+            () =>
+            {
+                RuleFor(orderItem => orderItem.OrderId).NotEqual(Guid.Empty);
+                RuleFor(orderItem => orderItem.ProductId).NotEqual(Guid.Empty);
+                RuleFor(orderItem => orderItem.Quantity).GreaterThan(0);
+                RuleFor(orderItem => orderItem.TotalPrice).GreaterThan(0);
+            });
+
+        RuleSet(
+            EntityEvent.OnUpdate.ToString(),
+            () =>
+            {
+                RuleFor(orderItem => orderItem.ProductId).NotEqual(Guid.Empty);
+                RuleFor(orderItem => orderItem.Quantity).GreaterThan(0);
+            });
+    }
+}
