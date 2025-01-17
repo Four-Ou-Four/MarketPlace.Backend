9560a346e48454807f7def735b2d7051e22cda9e
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-17 19:41:51 +0500

    Created order mapper

---
+++ MarketPlace.Infrastructure/Orders/Mappers/OrderMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Orders.Models;
+using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Infrastructure.Orders.Mappers;
+
+public class OrderMapper : Profile
+{
+    public OrderMapper()
+    {
+        CreateMap<Order, OrderDto>().ReverseMap();
+        CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+    }
+}
