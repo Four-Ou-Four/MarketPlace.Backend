fafae8d5dc3e9dfb91f4d391a43362435b248d59
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-17 19:41:27 +0500

    Created order query handlers

---
+++ MarketPlace.Infrastructure/Orders/QueryHandlers/OrderGetByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Orders.Models;
+using MarketPlace.Application.Orders.Queries;
+using MarketPlace.Application.Orders.Services;
+using MarketPlace.Domain.Common.Queries;
+
+namespace MarketPlace.Infrastructure.Orders.QueryHandlers;
+
+public class OrderGetByIdQueryHandler(
+    IMapper mapper,
+    IOrderService orderService)
+    : IQueryHandler<OrderGetByIdQuery, OrderDto>
+{
+    public async Task<OrderDto> Handle(OrderGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var result = await orderService.GetByIdAsync(request.OrderId, cancellationToken: cancellationToken);
+
+        return mapper.Map<OrderDto>(result);
+    }
+}
---
+++ MarketPlace.Infrastructure/Orders/QueryHandlers/OrderGetQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MarketPlace.Application.Orders.Models;
+using MarketPlace.Application.Orders.Queries;
+using MarketPlace.Application.Orders.Services;
+using MarketPlace.Domain.Common.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Infrastructure.Orders.QueryHandlers;
+
+public class OrderGetQueryHandler(
+    IMapper mapper,
+    IOrderService orderService)
+    : IQueryHandler<OrderGetQuery, ICollection<OrderDto>>
+{
+    public async Task<ICollection<OrderDto>> Handle(OrderGetQuery request, CancellationToken cancellationToken)
+    {
+        var result = await orderService.Get(
+            request.OrderFilter,
+            new QueryOptions()
+            {
+                QueryTrackingMode = QueryTrackingMode.AsNoTracking
+            })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<OrderDto>>(result);
+    }
+}
