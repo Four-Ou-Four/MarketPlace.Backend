4b43cd7dc9062502d37b5a7a6ea8cd90cabb8c34
Habiba Sattorova <habibasattorova26@gmail.com>
2025-01-17 19:39:57 +0500

    Created order validator

---
+++ MarketPlace.Infrastructure/Orders/Validators/OrderValidator.cs
@@ -0,0 +1,11 @@
+﻿using MarketPlace.Domain.Entities;
+using FluentValidation;
+
+namespace MarketPlace.Infrastructure.Orders.Validators;
+public class OrderValidator : AbstractValidator<Order>
+{
+    public OrderValidator()
+    {
+        RuleFor(order => order.UserId).NotEqual(Guid.Empty);
+    }
+}
